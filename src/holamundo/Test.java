ac40c26b715e6752906bb2866f99c227ebcf5f9d
Charly Cimino <carlosecimino@gmail.com>
2023-03-17 15:39:15 -0300

    Clase 1

---
+++ src/holamundo/Test.java
@@ -0,0 +1,19 @@
+
+package holamundo;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo " + "java");
+    }
+
+}
